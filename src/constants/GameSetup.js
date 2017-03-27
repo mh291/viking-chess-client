58f7f1bafc4685337761a74ca7fbcb5355008ff9
Michael Han <michael.mj.han@gmail.com>
2017-03-26 22:10:21 -0700

    Fix typo in instructions

--- src/constants/GameSetup.js
+++ src/constants/GameSetup.js
@@ -50,7 +50,7 @@ export const INIT_GAME_SETUP = [
 ];
 
 export const INSTRUCTIONS = <span>This is a modified version of viking chess. There are two players controlling black and white pieces. Black moves first and 
-                            then the  <br/>players alternate.Each piece moves vertically and horizontally as long as there are no obstacles in the way such
+                            then the  <br/>players alternate. Each piece moves vertically and horizontally as long as there are no obstacles in the way such
                             as another piece <br/>or the edge of the board. The goal of the black player is to capture the white king. The goal of the white 
-                            player is to move the white <br/>king to one of the four corners. These are safe zones and will the game will end if the king 
+                            player is to move the white <br/>king to one of the four corners. These are safe zones the game will end if the king 
                             reaches any of these spaces marked with<br/>  an X. Black pieces cannot move into a safe zone.</span>
